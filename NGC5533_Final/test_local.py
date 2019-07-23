06db10f5b9c1199fc22027512abc279f866f5bb5
Kitty Harris <kathryn.harris@ucdenver.edu>
2019-07-23 16:31:20 -0600

    Setting up travis CI correctly

---
+++ NGC5533_Final/test_local.py
@@ -0,0 +1,21 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--ExecutePreprocessor.kernel_name=python3",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+    print('Testing Jupyter notebooks...')
+    _exec_notebook('BlackHole_Velocity.ipynb')
+    #_exec_notebook('Bulge_RotationCurve_n2_7.ipynb')
+    #_exec_notebook('Disk_Velocity_kpc.ipynb')
+    #_exec_notebook('Halo_Velocity.ipynb')
+    #_exec_notebook('fitting_errors.ipynb')
+    #_exec_notebook('NGC5533_Total.ipynb')
