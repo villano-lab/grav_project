b69fad8e5a2e0c6e31232cf7b2c76d108fe4071c
Kitty Harris <kathryn.harris@ucdenver.edu>
2019-09-02 12:07:08 -0600

    Meeting updates 2 Sept 19

--- python/NGC5533_functions.py
+++ python/NGC5533_functions.py
@@ -111,10 +111,10 @@ def load(dataset,group,path='Inputs',file='x'+str(x[0])+'-'+str(x[len(x)-1])+'_'
 ######### Black Hole ###########
 ################################
 
-def bh_v(r,M=Mbh_def,save=True): #M in solar masses, r in kpc
+def bh_v(r,M=Mbh_def,save=False,load=False,**kwargs): #M in solar masses, r in kpc
     return np.sqrt(G*M/r)
     if save is True:
-        a = np.sqrt(G*M/x)
+        a = np.sqrt(G*M/r)
         save(a,'Mbh'+str(M),'blackhole')
 
 ################################
@@ -143,9 +143,10 @@ def b_vsquare(r,n=n_c):
     h = lambda m,r,n: C(n)*b_innerintegral(m,n)*(m**2)/(np.sqrt((r**2)-((m**2)*(e2)))) #integrate outer function
     return si.quad(h, 0, r, args=(r,n))[0]
 def b_vsquarev(r,n=n_c):
-    return np.vectorize(b_vsquare, otypes=[np.float])
+    a = np.vectorize(b_vsquare, otypes=[np.float])
+    return a(r,n)
 def b_v(r,n=n_c):
-    return b_vsquare(r,n)**(1/2)
+    return np.sqrt(b_vsquarev(r,n))
     #unvec = lambda r,n: b_vsquare(r,n)**(1/2)
     #return np.vectorize(unvec, otypes=[np.float])
 
@@ -166,7 +167,7 @@ def h_vcasertano(r,z,rc=h_rc,rho00=h_rho00,gamma=h_gamma):                      
 def h_vjimenez(r,rc=h_rc,rho00=h_rho00):
     return np.sqrt(4*np.pi*G*rho00*(rc**2)*(1-((rc/r)*np.arctan(r/rc))))
 
-def h_v(r,save=True):
+def h_vNFW(r,save=True):
     rho = lambda r: rho_s/((r/rs)*((1+r/rs)**2))
     f = lambda R: 4*np.pi*rho(R)*(R**2)          #NFW Density Profile
     mdm = lambda r: si.quad(f, 0, r)[0]          #M(r)
@@ -176,6 +177,11 @@ def h_v(r,save=True):
     if save is True:
         savedata()
 
+def h_viso(r,rc=h_rc,rho00=h_rho00):
+    return np.sqrt(4*np.pi*G*rho00*(rc**2)*(1-((rc/r)*np.arctan(r/rc))))
+h_v = h_viso
+        
+        
 ################################
 ############ Disk ##############
 ################################
