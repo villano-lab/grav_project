Fixed error in black hole calculation that was causing fitting issues.

python/NGC5533_functions.py
@@ -151,11 +151,10 @@ def bh_v(r,M=Mbh_def,save=False,load=False,**kwargs): #M in solar masses, r in k
             y = loaddata('blackhole','Mbh'+str(M),**kwargs)[1]
             x = loaddata('blackhole','Mbh'+str(M),**kwargs)[0]
         except KeyError: #If unable to load, load default instead and apply a prefactor retroactively
-            y = M*loaddata('blackhole','Mbh1',**kwargs)[1]
+            y = np.sqrt(M)*loaddata('blackhole','Mbh1',**kwargs)[1]
             x = loaddata('blackhole','Mbh1',**kwargs)[0]
-        a = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
-        return a(r)
-        return loaddata('blackhole','Mbh'+str(M),**kwargs)
+        spline = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
+        return spline(r)
     else:
         return a
     