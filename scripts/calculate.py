c2f09fbe9e5d34dd26d71e1a45f9ae16208ee4f7
Kitty Harris <kathryn.harris@ucdenver.edu>
2019-10-16 17:30:51 -0600

    Still running, has been running for at least a day

--- scripts/calculate.py
+++ scripts/calculate.py
@@ -1,12 +1,15 @@
-import ../python/NGC5533_Functions as nf
+import sys
+sys.path.append('/home/gerudo7/git_repositories/grav_project/python/')
+import NGC5533_functions as nf
+import numpy as np
 
-x = np.linspace(0,20,100)
+x = np.linspace(0.01,20,100)
 rdat = np.linspace(0,125,100)
 
 #For every pair of numbers in x,
 for k in x:
     for j in x:
         #calculate and save the disk and bulge values.
-        nf.d_v(rdat,k,j,save=True,path='../fitting')
-        nf.b_v(rdat,k,j,save=True,path='../fitting')
-        print(str(j)+':'+str(k)+' (maximum = 20:20)  ', end='', flush=True)
+        nf.d_v(rdat,k,j,load=True,path='../fitting/')
+        nf.b_v(rdat,k,j,load=True,path='../fitting/')
+        print(str(j)+':'+str(k)+' (maximum = 20:20)  ')
