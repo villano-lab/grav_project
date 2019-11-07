d6bc342479a100985ddc8781038b198fed103406
Kitty Harris <kathyrn.harris@ucdenver.edu>
2019-11-07 12:18:18 -0700

    Fixing errors from rearranging file structure

--- python/NGC5533_functions.py
+++ python/NGC5533_functions.py
@@ -97,8 +97,8 @@ def savedata(xvalues,yvalues,group,dataset,path='./',file='Inputs.hdf5'):
                 del grp[dataset]
                 savedata(x,y,group,dataset,path,file)
                 return y
-            finally: #No matter what,
-                saved.close()
+        finally: #No matter what,
+            saved.close()
         #print("Saved.") #Convenient for debugging but annoying for fitting.
     elif h5py == 0:
         print("ERROR: h5py was not loaded.")
@@ -130,6 +130,7 @@ def loaddata(group,dataset,path='./',file='Inputs.hdf5'):
     elif h5py == 0:
         print("ERROR: h5py was not loaded.")
         return 1
+    saved.close() #no matter what, close the file when you're done
     
 def checkfile(group='all',path='./',file='Inputs.hdf5'):
     if h5py ==1:
@@ -161,7 +162,7 @@ def checkfile(group='all',path='./',file='Inputs.hdf5'):
 ######### Black Hole ###########
 ################################
 
-def bh_v(r,M=Mbh_def,save=False,load=True,grp='blackhole',**kwargs): #M in solar masses, r in kpc
+def bh_v(r,M=Mbh_def,save=False,load=True,comp='blackhole',**kwargs): #M in solar masses, r in kpc
     if isinstance(r,float) or isinstance(r,int):
         r = np.asarray([r])
     if isinstance(r,list):
@@ -169,11 +170,13 @@ def bh_v(r,M=Mbh_def,save=False,load=True,grp='blackhole',**kwargs): #M in solar
     a = np.sqrt(G*M/r)
     if load:
         try: #Load existing prefactor if available
-            y = loaddata(grp,'Mbh'+str(M),file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'Mbh'+str(M),file=grp+'.hdf5',**kwargs)[0]
+            #file = comp+'.hdf5'
+            #print(file) #Troubleshooting
+            y = loaddata(comp,'Mbh'+str(M),file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'Mbh'+str(M),file=comp+'.hdf5',**kwargs)[0]
         except KeyError: #If unable to load, load default instead and apply a prefactor retroactively
-            y = np.sqrt(M)*loaddata(grp,'Mbh1',file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'Mbh1',file=grp+'.hdf5',**kwargs)[0]
+            y = np.sqrt(M)*loaddata(comp,'Mbh1',file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'Mbh1',file=comp+'.hdf5',**kwargs)[0]
             spline = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
             return spline(r)
         except: #Attempting to catch problem with spline having too few points
@@ -183,7 +186,7 @@ def bh_v(r,M=Mbh_def,save=False,load=True,grp='blackhole',**kwargs): #M in solar
             print(sys.exc_info()[2])
             save = True #Calculate since there aren't enough points
     if save:
-        savedata(r,a,grp,'Mbh'+str(M),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'Mbh'+str(M),file=comp+'.hdf5',**kwargs)
         return a
     else:
         return a
@@ -217,13 +220,13 @@ def b_vsquarev(r,n=n_c,re=re_c):
     a = np.vectorize(b_vsquare)
     return a(r,n,re)
 
-def b_v(r,n=n_c,re=re_c,save=False,load=True,grp='bulge',**kwargs):
+def b_v(r,n=n_c,re=re_c,save=False,load=True,comp='bulge',**kwargs):
     if isinstance(r,float) or isinstance(r,int):
         r = np.asarray([r])
     if load:
         try: #load if exists
-            y = loaddata(grp,'n'+str(n)+'re'+str(re),file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'n'+str(n)+'re'+str(re),file=grp+'.hdf5',**kwargs)[0]
+            y = loaddata(comp,'n'+str(n)+'re'+str(re),file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'n'+str(n)+'re'+str(re),file=comp+'.hdf5',**kwargs)[0]
             b = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
             return b(r)
         except KeyError: #if does not exist,
@@ -237,7 +240,7 @@ def b_v(r,n=n_c,re=re_c,save=False,load=True,grp='bulge',**kwargs):
     a = b_vsquarev(r,n,re)**(1/2)
     a[np.isnan(a)] = 0
     if save:
-        savedata(r,a,grp,'n'+str(n)+'re'+str(re),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'n'+str(n)+'re'+str(re),file=comp+'.hdf5',**kwargs)
         return a
     else:
         return a
@@ -268,14 +271,14 @@ def h_vNFW(r,save=True,**kwargs):
     a = np.sqrt(vdm2v(r))
     a[np.isnan(a)] = 0
     if save:
-        savedata(r,a,grp,'n'+str('PLACEHOLDER'),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'n'+str('PLACEHOLDER'),file=comp+'.hdf5',**kwargs)
         return a
     elif load:
-        return loaddata(grp,'n'+str('PLACEHOLDER'),file=grp+'.hdf5',**kwargs)
+        return loaddata(comp,'n'+str('PLACEHOLDER'),file=comp+'.hdf5',**kwargs)
     else:
         return a(r)
 
-def h_viso(r,rc=h_rc,rho00=hrho00_c,load=True,save=False,grp='halo',**kwargs):   #h_v iso
+def h_viso(r,rc=h_rc,rho00=hrho00_c,load=True,save=False,comp='halo',**kwargs):   #h_v iso
     if isinstance(r,float) or isinstance(r,int):
         r = np.asarray([r])
     a = np.zeros(len(r))
@@ -286,8 +289,8 @@ def h_viso(r,rc=h_rc,rho00=hrho00_c,load=True,save=False,grp='halo',**kwargs):  
     a[np.isnan(a)] = 0
     if load:
         try: #Load if exists
-            y = loaddata(grp,'rc'+str(rc)+'rho00'+str(rho00),file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'rc'+str(rc)+'rho00'+str(rho00),file=grp+'.hdf5',**kwargs)[0]
+            y = loaddata(comp,'rc'+str(rc)+'rho00'+str(rho00),file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'rc'+str(rc)+'rho00'+str(rho00),file=comp+'.hdf5',**kwargs)[0]
             b = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
             return b(r)
         except KeyError: #If does not exist,
@@ -299,7 +302,7 @@ def h_viso(r,rc=h_rc,rho00=hrho00_c,load=True,save=False,grp='halo',**kwargs):  
             print(sys.exc_info()[2])
             save = True #Calculate since there aren't enough points
     if save:
-        savedata(r,a,grp,'rc'+str(rc)+'rho00'+str(rho00),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'rc'+str(rc)+'rho00'+str(rho00),file=comp+'.hdf5',**kwargs)
         return a
     else:
         return a
@@ -385,21 +388,21 @@ def d_F(r,h=h_c,d_rho00=drho00_c,pref=1): #multiplying by upsylon. Generally we 
         return val
 d_Fv = np.vectorize(d_F)
 
-def d_v(r,h=h_c,d_rho00=drho00_c,pref=1,save=False,load=True,grp='disk',**kwargs): #velocity
+def d_v(r,h=h_c,d_rho00=drho00_c,pref=1,save=False,load=True,comp='disk',**kwargs): #velocity
     if isinstance(r,float) or isinstance(r,int):
         r = np.asarray([r])
     if isinstance(r,list):
         r = np.asarray(r)
     if load:
         try: #Load existing prefactor if available
-            y = loaddata(grp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=grp+'.hdf5',**kwargs)[0]
+            y = loaddata(comp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=comp+'.hdf5',**kwargs)[0]
             b = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
             return b(r)
         except KeyError: #If unable to load, load 1 instead and apply a prefactor retroactively
             try:
-                y = pref*loaddata(grp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref1',grp=group,file=grp+'.hdf5',**kwargs)[1]
-                x = loaddata(grp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref1',file=grp+'.hdf5',**kwargs)[0]
+                y = pref*loaddata(comp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref1',comp=group,file=comp+'.hdf5',**kwargs)[1]
+                x = loaddata(comp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref1',file=comp+'.hdf5',**kwargs)[0]
                 b = inter.InterpolatedUnivariateSpline(x,y,k=3) #k is the order of the polynomial
                 return b(r)
             except KeyError: #And if still unable to load, calculate and save.
@@ -414,7 +417,7 @@ def d_v(r,h=h_c,d_rho00=drho00_c,pref=1,save=False,load=True,grp='disk',**kwargs
         r = np.asarray(r)
         a = np.sqrt(-r*d_Fv(r,h,d_rho00,pref))
         a[np.isnan(a)] = 0
-        savedata(r,a,grp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref),file=comp+'.hdf5',**kwargs)
         return a
     else:
         a = np.sqrt(-r*d_Fv(r,h,d_rho00,pref))
@@ -424,15 +427,15 @@ def d_v(r,h=h_c,d_rho00=drho00_c,pref=1,save=False,load=True,grp='disk',**kwargs
 ################################
 ############ Total #############
 ################################
-def v(r,M=Mbh_def,re=re_c,h=h_c,d_rho00=drho00_c,pref=1,rc=h_rc,h_rho00=hrho00_c,save=False,load=True,grp='total',**kwargs): 
+def v(r,M=Mbh_def,re=re_c,h=h_c,d_rho00=drho00_c,pref=1,rc=h_rc,h_rho00=hrho00_c,save=False,load=True,comp='total',**kwargs): 
     if isinstance(r,float) or isinstance(r,int):
         r = np.asarray([r])
     a = np.sqrt(np.sqrt(h_v(r,rc,h_rho00)**2+d_v(r,h,d_rho00,pref)**2+bh_v(r,M)**2+b_v(r,re)**2))
     a[np.isnan(a)] = 0
     if load:
         try: #Load if exists
-            y = loaddata(grp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00), file=grp+'.hdf5',**kwargs)[1]
-            x = loaddata(grp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00), file=grp+'.hdf5', **kwargs)[0]
+            y = loaddata(comp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00), file=comp+'.hdf5',**kwargs)[1]
+            x = loaddata(comp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00), file=comp+'.hdf5', **kwargs)[0]
             b = inter.InterpolatedUnivariateSpline(x,y,k=3)
             return b(r)
         except KeyError: #If does not exist,
@@ -444,7 +447,7 @@ def v(r,M=Mbh_def,re=re_c,h=h_c,d_rho00=drho00_c,pref=1,rc=h_rc,h_rho00=hrho00_c
             print(sys.exc_info()[2])
             save = True #Calculate since there aren't enough points
     if save: #not elif since that would mean don't check if load was true, which I don't want in this case
-        savedata(r,a,grp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00),file=grp+'.hdf5',**kwargs)
+        savedata(r,a,comp,'Mbh'+str(M)+'re'+str(re)+'h'+str(h)+'d_rho00'+str(d_rho00)+'pref'+str(pref) +'rc'+str(rc)+'h_rho00'+str(h_rho00),file=comp+'.hdf5',**kwargs)
         return a
     elif not load: #If load was false,
         return a
