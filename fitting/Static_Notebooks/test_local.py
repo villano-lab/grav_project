4becc08d97708ff8561ac4398eacdce7c3d1bc8c
Kitty <kathryn.harris@ucdenver.edu>
2020-03-09 18:06:29 -0600

    Expanding testing

--- fitting/Static_Notebooks/test_local.py
+++ fitting/Static_Notebooks/test_local.py
@@ -18,4 +18,10 @@ def test():
     _exec_notebook('Fitting-Bulge.ipynb')
     _exec_notebook('Fitting-Disk.ipynb')
     _exec_notebook('FittingB-D.ipynb')
+    #_exec_notebook('FittingB-H.ipynb') #This one will not enter testing because it takes too long.
+    #Iff the bulge process is sped up then it can be added. 
+    #It does, however, still need to finish a complete run so that runtime is up-to-date.
+    #
+    #_exec_notebook('FittingBH-B.ipynb) #Still waiting to run this; bulge file needs to be available.
+    _exec_notebook('FittingBH-D.ipynb)
     _exec_notebook('NGC5533_Noordermeer_plot.ipynb')
