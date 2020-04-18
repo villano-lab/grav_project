2eee78124e0e9343e2d18c6e25636d846b567a8a
Kitty <kathryn.harris@ucdenver.edu>
2020-04-18 15:03:22 -0600

    Re-running locally and removing comments to locate error in testing.

--- fitting/Static_Notebooks/test_local.py
+++ fitting/Static_Notebooks/test_local.py
@@ -18,14 +18,8 @@ def test():
     _exec_notebook('Fitting-Bulge.ipynb')
     _exec_notebook('Fitting-Disk.ipynb')
     _exec_notebook('FittingB-D.ipynb')
-    #_exec_notebook('FittingB-H.ipynb') #This one will not enter testing because it takes too long and the bulge file has reached maximum size.
-    #_exec_notebook('FittingBH-B.ipynb) #This one will not enter testing because it takes too long and the bulge file has reached maximum size.
-    #Iff the bulge process is sped up then the above can be added.
-    #
     _exec_notebook('FittingBH-D.ipynb')
-    #_exec_notebook('FittingBH-H_different_galaxies.ipynb') #Getting encoding error, not sure why.
     _exec_notebook('FittingBH-H.ipynb')
-    #_exec_notebook('FittingD-H.ipynb') #Did not finish running within 3 days and halo file has reached maximum size. Can be re-attempted if disk is sped up.
     _exec_notebook('NGC5533_Noordermeer_plot-tweaked.ipynb')
     _exec_notebook('NGC5533_Noordermeer_plot.ipynb')
-    _exec_notebook('time setup.ipynb') #Not something that should break, but will flag if time library updates break anything.
+    _exec_notebook('time setup.ipynb')
