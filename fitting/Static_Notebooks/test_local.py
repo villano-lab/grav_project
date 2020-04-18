bcd45d70beb44a430a6353b5a376134d6da5ee9f
Kitty <kathryn.harris@ucdenver.edu>
2020-04-18 13:26:37 -0600

    Finished going through what can currently be added to testing.

--- fitting/Static_Notebooks/test_local.py
+++ fitting/Static_Notebooks/test_local.py
@@ -18,14 +18,14 @@ def test():
     _exec_notebook('Fitting-Bulge.ipynb')
     _exec_notebook('Fitting-Disk.ipynb')
     _exec_notebook('FittingB-D.ipynb')
-    #_exec_notebook('FittingB-H.ipynb') #This one will not enter testing because it takes too long.
-    #_exec_notebook('FittingBH-B.ipynb) #Takes too long to run.
+    #_exec_notebook('FittingB-H.ipynb') #This one will not enter testing because it takes too long and the bulge file has reached maximum size.
+    #_exec_notebook('FittingBH-B.ipynb) #This one will not enter testing because it takes too long and the bulge file has reached maximum size.
     #Iff the bulge process is sped up then the above can be added.
     #
     _exec_notebook('FittingBH-D.ipynb')
     #_exec_notebook('FittingBH-H_different_galaxies.ipynb') #Getting encoding error, not sure why.
-    #_exec_notebook('FittingBH-H.ipynb') #Still running.
-    #_exec_notebook('FittingD-H.ipynb') #Did not finish running. Gave up on this one after three days straight of computation. Can be re-attempted if disk is sped up.
+    _exec_notebook('FittingBH-H.ipynb')
+    #_exec_notebook('FittingD-H.ipynb') #Did not finish running within 3 days and halo file has reached maximum size. Can be re-attempted if disk is sped up.
     #_exec_notebook('NGC5533_Noordermeer_plot-tweaked.ipynb') #Still waiting to run this; halo file needs to be available.
     _exec_notebook('NGC5533_Noordermeer_plot.ipynb')
     _exec_notebook('time setup.ipynb') #Not something that should break, but will flag if time library updates break anything.
