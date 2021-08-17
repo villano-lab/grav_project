aa9f880559ba713c5246df3b3a8c727f51c0663b
Judit Bergfalk <juditbergfalk@gmail.com>
2021-08-17 16:46:39 -0600

    [JB] added measured data of multiple galaxies

--- workshop/python/load_galaxies.py
+++ workshop/python/load_galaxies.py
@@ -12,6 +12,15 @@ import scipy.interpolate  as inter
 import matplotlib.pyplot  as plt
 
 ###############################
+###### Dark Matter Halo #######
+###############################
+
+# Equation for dark matter halo velocity
+def halo_v(r,rho0,rc):
+    v = np.sqrt(4*np.pi*G*rho0*rc**2*(1 - rc/r * np.arctan(r/rc)))
+    return v
+
+###############################
 ########### NGC5533 ###########
 ###############################
 
@@ -32,7 +41,13 @@ NGC5533 = {
     'measured_data': dp.getXYdata_wXYerr('data/NGC5533/100kpc_data.txt')
         
 }
-    
+
+# Parameters ########################
+NGC5533['galaxyname'] = 'NGC 5533'   # NGC catalog number of the galaxy
+NGC5533['rho0'] = 0.31e9       # central mass density (in solar mass/kpc^3), Source: Noordermeer (2007)     
+NGC5533['rc'] = 1.4            # core radius (in kpc), Source: Noordermeer (2007)
+NGC5533['massbh'] = 2.7e9      # mass of central black hole (in solar masses), Source: Noordermeer (2007)
+
 #Organize 100kpc data
 NGC5533['m_radii']      = np.asarray(NGC5533['measured_data']['xx'])
 NGC5533['m_velocities'] = np.asarray(NGC5533['measured_data']['yy'])
@@ -102,22 +117,240 @@ NGC5533['gas'] = {
 NGC5533['gas']['t'], NGC5533['gas']['c'], NGC5533['gas']['k'] = inter.splrep(NGC5533['gas']['r'], NGC5533['gas']['v'])
 NGC5533['gas']['spline'] = inter.BSpline(NGC5533['gas']['t'], NGC5533['gas']['c'], NGC5533['gas']['k'])
 
-# Parameters ########################
-NGC5533['galaxyname'] = 5533       # NGC catalog number of the galaxy
-NGC5533['rho0'] = 0.31e9       # central mass density (in solar mass/kpc^3), Source: Noordermeer (2007)     
-NGC5533['rc'] = 1.4            # core radius (in kpc), Source: Noordermeer (2007)
-NGC5533['massbh'] = 2.7e9      # mass of central black hole (in solar masses), Source: Noordermeer (2007)
 
 ###############################
 ########### NGC0891 ###########
 ###############################
 
 NGC0891 = {
-    'measured_data' : dp.getXYdata_wXYerr('data/NGC891/891_data')
+    
+    'raw_bulge'        : dp.getXYdata('data/NGC0891/891_dtBulge.dat'     ),
+    'raw_disk'         : dp.getXYdata('data/NGC0891/891_dtDisk.dat'      ),
+    # missing: 'raw_halo'         : dp.getXYdata('data/NGC0891/891_dtHalo.dat'      ),
+    'raw_gas'          : dp.getXYdata('data/NGC0891/891_dtGas.dat'       ),
+
+    # Get data
+    'measured_data' : dp.getXYdata_wXYerr('data/NGC0891/891_data')
 }
+
+# Parameters ########################
+NGC0891['galaxyname'] = 'NGC 891'    # NGC catalog number of the galaxy
+NGC0891['rho0'] = 3.31e7       # central mass density (in solar mass/kpc^3), Source: Richards et al. (2015)    
+NGC0891['rc'] = 1.9            # core radius (in kpc), Source: Richards et al. (2015)
+NGC0891['massbh'] = 0          # mass of central black hole (in solar masses)
 
 #Organize measured data
 NGC0891['m_radii']      = NGC0891['measured_data']['xx']
 NGC0891['m_velocities'] = NGC0891['measured_data']['yy']
 NGC0891['m_r_errors']   = NGC0891['measured_data']['ex']
 NGC0891['m_v_errors']   = NGC0891['measured_data']['ey']
+
+# Bulge #############################
+NGC0891['bulge'] = {
+    'r' : np.asarray(NGC0891['raw_bulge']['xx']),
+    'v' : np.asarray(NGC0891['raw_bulge']['yy'])
+}
+NGC0891['bulge']['t'], NGC0891['bulge']['c'], NGC0891['bulge']['k'] = inter.splrep(NGC0891['bulge']['r'], NGC0891['bulge']['v'])
+NGC0891['bulge']['spline'] = inter.BSpline(NGC0891['bulge']['t'], NGC0891['bulge']['c'], NGC0891['bulge']['k'])
+
+# Disk ##############################
+NGC0891['disk'] = {
+    'r' : np.asarray(NGC0891['raw_disk']['xx']),
+    'v' : np.asarray(NGC0891['raw_disk']['yy'])
+}
+NGC0891['disk']['t'], NGC0891['disk']['c'], NGC0891['disk']['k'] = inter.splrep(NGC0891['disk']['r'], NGC0891['disk']['v'])
+NGC0891['disk']['spline'] = inter.BSpline(NGC0891['disk']['t'], NGC0891['disk']['c'], NGC0891['disk']['k'])
+
+# Halo ##############################
+NGC0891['halo'] = lambda r: halo_v(r,NGC0891['rho0'],NGC0891['rc'])
+
+# Gas ###############################
+NGC0891['gas'] = {
+    'r' : np.asarray(NGC0891['raw_gas']['xx']),
+    'v' : np.asarray(NGC0891['raw_gas']['yy'])
+}
+NGC0891['gas']['t'], NGC0891['gas']['c'], NGC0891['gas']['k'] = inter.splrep(NGC0891['gas']['r'], NGC0891['gas']['v'])
+NGC0891['gas']['spline'] = inter.BSpline(NGC0891['gas']['t'], NGC0891['gas']['c'], NGC0891['gas']['k'])
+
+
+###############################
+########### NGC7814 ###########
+###############################
+
+NGC7814 = {
+    
+    'raw_bulge'        : dp.getXYdata('data/NGC7814/7814reallybulge.dat'     ),
+    'raw_disk'         : dp.getXYdata('data/NGC7814/7814reallydisk.dat'      ),
+    # missing: 'raw_halo'         : dp.getXYdata('data/NGC7814/7814reallyhalo.dat'      ),
+    'raw_gas'          : dp.getXYdata('data/NGC7814/7814reallygas.dat'       ),
+
+    # Get data
+    'measured_data' : dp.getXYdata_wXYerr('data/NGC7814/ngc7814data')
+}
+
+# Parameters ########################
+NGC7814['galaxyname'] = 'NGC 7814'   # NGC catalog number of the galaxy
+NGC7814['rho0'] = 1.52e8       # central mass density (in solar mass/kpc^3), Source: Richards et al. (2015)
+NGC7814['rc'] = 2.1            # core radius (in kpc), Source: Richards et al. (2015)
+NGC7814['massbh'] = 0          # mass of central black hole (in solar masses)
+
+#Organize measured data
+NGC7814['m_radii']      = NGC7814['measured_data']['xx']
+NGC7814['m_velocities'] = NGC7814['measured_data']['yy']
+NGC7814['m_r_errors']   = NGC7814['measured_data']['ex']
+NGC7814['m_v_errors']   = NGC7814['measured_data']['ey']
+
+# Bulge #############################
+NGC7814['bulge'] = {
+    'r' : np.asarray(NGC7814['raw_bulge']['xx']),
+    'v' : np.asarray(NGC7814['raw_bulge']['yy'])
+}
+NGC7814['bulge']['t'], NGC7814['bulge']['c'], NGC7814['bulge']['k'] = inter.splrep(NGC7814['bulge']['r'], NGC7814['bulge']['v'])
+NGC7814['bulge']['spline'] = inter.BSpline(NGC7814['bulge']['t'], NGC7814['bulge']['c'], NGC7814['bulge']['k'])
+
+# Disk ##############################
+NGC7814['disk'] = {
+    'r' : np.asarray(NGC7814['raw_disk']['xx']),
+    'v' : np.asarray(NGC7814['raw_disk']['yy'])
+}
+NGC7814['disk']['t'], NGC7814['disk']['c'], NGC7814['disk']['k'] = inter.splrep(NGC7814['disk']['r'], NGC7814['disk']['v'])
+NGC7814['disk']['spline'] = inter.BSpline(NGC7814['disk']['t'], NGC7814['disk']['c'], NGC7814['disk']['k'])
+
+# Halo ##############################
+NGC7814['halo'] = lambda r: halo_v(r,NGC0891['rho0'],NGC0891['rc'])
+
+# Gas ###############################
+NGC7814['gas'] = {
+    'r' : np.asarray(NGC7814['raw_gas']['xx']),
+    'v' : np.asarray(NGC7814['raw_gas']['yy'])
+}
+NGC7814['gas']['t'], NGC7814['gas']['c'], NGC7814['gas']['k'] = inter.splrep(NGC7814['gas']['r'], NGC7814['gas']['v'])
+NGC7814['gas']['spline'] = inter.BSpline(NGC7814['gas']['t'], NGC7814['gas']['c'], NGC7814['gas']['k'])
+
+
+###############################
+########### NGC5005 ###########
+###############################
+
+NGC5005 = {
+    
+    'raw_bulge'        : dp.getXYdata('data/NGC5005/ngc5005_bulge.txt'     ),
+    'raw_disk'         : dp.getXYdata('data/NGC5005/ngc5005_disk.txt'      ),
+    'raw_halo'         : dp.getXYdata('data/NGC5005/ngc5005_halo.txt'      ),
+    'raw_gas'          : dp.getXYdata('data/NGC5005/ngc5005_gas.txt'       ),
+
+    # Get data
+    'measured_data' : dp.getXYdata_wXYerr('data/NGC5005/ngc5005_data.txt')
+}
+
+#Organize measured data
+NGC5005['m_radii']      = NGC5005['measured_data']['xx']
+NGC5005['m_velocities'] = NGC5005['measured_data']['yy']
+NGC5005['m_r_errors']   = NGC5005['measured_data']['ex']
+NGC5005['m_v_errors']   = NGC5005['measured_data']['ey']
+
+# Bulge #############################
+NGC5005['bulge'] = {
+    'r' : np.asarray(NGC5005['raw_bulge']['xx']),
+    'v' : np.asarray(NGC5005['raw_bulge']['yy'])
+}
+NGC5005['bulge']['t'], NGC5005['bulge']['c'], NGC5005['bulge']['k'] = inter.splrep(NGC5005['bulge']['r'], NGC5005['bulge']['v'])
+NGC5005['bulge']['spline'] = inter.BSpline(NGC5005['bulge']['t'], NGC5005['bulge']['c'], NGC5005['bulge']['k'])
+
+# Disk ##############################
+NGC5005['disk'] = {
+    'r' : np.asarray(NGC5005['raw_disk']['xx']),
+    'v' : np.asarray(NGC5005['raw_disk']['yy'])
+}
+NGC5005['disk']['t'], NGC5005['disk']['c'], NGC5005['disk']['k'] = inter.splrep(NGC5005['disk']['r'], NGC5005['disk']['v'])
+NGC5005['disk']['spline'] = inter.BSpline(NGC5005['disk']['t'], NGC5005['disk']['c'], NGC5005['disk']['k'])
+
+# Halo ##############################
+NGC5005['halo'] = {
+    'r' : np.asarray(NGC5005['raw_halo']['xx']),
+    'v' : np.asarray(NGC5005['raw_halo']['yy'])
+}
+NGC5005['halo']['t'], NGC5005['halo']['c'], NGC5005['halo']['k'] = inter.splrep(NGC5005['halo']['r'], NGC5005['halo']['v'])
+NGC5005['halo']['spline'] = inter.BSpline(NGC5005['halo']['t'], NGC5005['halo']['c'], NGC5005['halo']['k'])
+
+# Gas ###############################
+NGC5005['gas'] = {
+    'r' : np.asarray(NGC5005['raw_gas']['xx']),
+    'v' : np.asarray(NGC5005['raw_gas']['yy'])
+}
+NGC5005['gas']['t'], NGC5005['gas']['c'], NGC5005['gas']['k'] = inter.splrep(NGC5005['gas']['r'], NGC5005['gas']['v'])
+NGC5005['gas']['spline'] = inter.BSpline(NGC5005['gas']['t'], NGC5005['gas']['c'], NGC5005['gas']['k'])
+
+# Parameters ########################
+NGC5005['galaxyname'] = 'NGC 5005'   # NGC catalog number of the galaxy
+NGC5005['rho0'] = 1.78e6    # central mass density (in solar mass/kpc^3), Source: Di Paolo et al. (2019): converted from g/cm^3    
+NGC5005['rc'] = 2.5         # core radius (in kpc), Source: Richards et al. (2015)
+NGC5005['massbh'] = 0       # mass of central black hole (in solar masses)
+
+
+###############################
+####### Other Galaxies ########
+###############################
+
+# UGC 89
+UGC89 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC89.txt')}
+UGC89['m_radii']      = UGC89['measured_data']['xx']
+UGC89['m_velocities'] = UGC89['measured_data']['yy']
+UGC89['m_v_errors']   = UGC89['measured_data']['ey']
+UGC89['galaxyname'] = 'UGC 89' 
+
+# UGC 477
+UGC477 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC477.txt')}
+UGC477['m_radii']      = UGC477['measured_data']['xx']
+UGC477['m_velocities'] = UGC477['measured_data']['yy']
+UGC477['m_v_errors']   = UGC477['measured_data']['ey']
+UGC477['galaxyname'] = 'UGC 477'
+
+# UGC 1281
+UGC1281 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC1281.txt')}
+UGC1281['m_radii']      = UGC1281['measured_data']['xx']
+UGC1281['m_velocities'] = UGC1281['measured_data']['yy']
+UGC1281['m_v_errors']   = UGC1281['measured_data']['ey']
+UGC1281['galaxyname'] = 'UGC 1281' 
+
+# UGC 1437
+UGC1437 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC1437.txt')}
+UGC1437['m_radii']      = UGC1437['measured_data']['xx']
+UGC1437['m_velocities'] = UGC1437['measured_data']['yy']
+UGC1437['m_v_errors']   = UGC1437['measured_data']['ey']
+UGC1437['galaxyname'] = 'UGC 1437' 
+
+# UGC 2953
+UGC2953 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC2953.txt')}
+UGC2953['m_radii']      = UGC2953['measured_data']['xx']
+UGC2953['m_velocities'] = UGC2953['measured_data']['yy']
+UGC2953['m_v_errors']   = UGC2953['measured_data']['ey']
+UGC2953['galaxyname'] = 'UGC 2953'
+
+# UGC 4325
+UGC4325 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC4325.txt')}
+UGC4325['m_radii']      = UGC4325['measured_data']['xx']
+UGC4325['m_velocities'] = UGC4325['measured_data']['yy']
+UGC4325['m_v_errors']   = UGC4325['measured_data']['ey']
+UGC4325['galaxyname'] = 'UGC 4325'
+
+# UGC 5253
+UGC5253 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC5253.txt')}
+UGC5253['m_radii']      = UGC5253['measured_data']['xx']
+UGC5253['m_velocities'] = UGC5253['measured_data']['yy']
+UGC5253['m_v_errors']   = UGC5253['measured_data']['ey']
+UGC5253['galaxyname'] = 'UGC 5253'   
+
+# UGC 6787
+UGC6787 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC6787.txt')}
+UGC6787['m_radii']      = UGC6787['measured_data']['xx']
+UGC6787['m_velocities'] = UGC6787['measured_data']['yy']
+UGC6787['m_v_errors']   = UGC6787['measured_data']['ey']
+UGC6787['galaxyname'] = 'UGC 6787'   
+
+# UGC 10075
+UGC10075 = {'measured_data' : dp.getXYdata_wYerr('data/othergalaxies/UGC10075.txt')}
+UGC10075['m_radii']      = UGC10075['measured_data']['xx']
+UGC10075['m_velocities'] = UGC10075['measured_data']['yy']
+UGC10075['m_v_errors']   = UGC10075['measured_data']['ey']
+UGC10075['galaxyname'] = 'UGC 10075'
