6ab1df918cbaa5d1ed9f4d5e585960e65323c925
Judit Bergfalk <juditbergfalk@gmail.com>
2021-08-19 17:35:13 -0600

    [JB] rotcurve animations are now displayed in notebook

--- workshop/python/rc_animation.py
+++ workshop/python/rc_animation.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 
-import seaborn as sns
-
 ###############################################
 
 def CalculatePosition(radius,velocity,time,dt):
@@ -180,56 +178,5 @@ def MakeAnimation(radius,velocity,time,dt,filename,title,
             
             # Save the current plot as a movie frame
             writer.grab_frame()
-            
-###############################################
-
-# create a figure and axes
-fig = plt.figure(figsize=(12,5))
-ax1 = plt.subplot(1,2,1)   
-ax2 = plt.subplot(1,2,2)
-
-# set up the subplots as needed
-ax1.set_xlim(( 0, 2))            
-ax1.set_ylim((-2, 2))
-ax1.set_xlabel('Time')
-ax1.set_ylabel('Magnitude')
-
-ax2.set_xlim((-2,2))
-ax2.set_ylim((-2,2))
-ax2.set_xlabel('X')
-ax2.set_ylabel('Y')
-ax2.set_title('Phase Plane')
-
-# create objects that will change in the animation. These are
-# initially empty, and will be given new values for each frame
-# in the animation.
-txt_title = ax1.set_title('')
-line1, = ax1.plot([], [], 'b', lw=2)     # ax.plot returns a list of 2D line objects
-line2, = ax1.plot([], [], 'r', lw=2)
-pt1, = ax2.plot([], [], 'g.', ms=20)
-line3, = ax2.plot([], [], 'y', lw=2)
-
-ax1.legend(['sin','cos']);
-
-# animation function. This is called sequentially
-def drawframe(n):
-    x = np.linspace(0, 2, 1000)
-    y1 = np.sin(2 * np.pi * (x - 0.01 * n))
-    y2 = np.cos(2 * np.pi * (x - 0.01 * n))
-    line1.set_data(x, y1)
-    line2.set_data(x, y2)
-    line3.set_data(y1[0:50],y2[0:50])
-    pt1.set_data(y1[0],y2[0])
-    txt_title.set_text('Frame = {0:4d}'.format(n))
-    return (line1,line2)
-
-
-
-from matplotlib import animation
-
-# blit=True re-draws only the parts that have changed.
-anim = animation.FuncAnimation(fig, drawframe, frames=100, interval=20, blit=True)
-
-
-
-
+        
+        plt.close(fig)    # Do not display the image
